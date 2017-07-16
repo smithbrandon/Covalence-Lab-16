daafcf774a46705e3822e8e404659458cb6a142c
Brandon Smith <smith.brandon.e.82@gmail.com>
2017-07-16 18:19:42 -0400

    removed heroku testing

--- server/index.js
+++ server/index.js
@@ -9,13 +9,12 @@ console.log("Starting web server at localhost:3000");
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_SCHEMA
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 });
 
-mysql://b13ba6edfe4c53:dcec68cb@us-cdbr-iron-east-03.cleardb.net/heroku_a0b6f77b3311813?reconnect=true
 var clientPath = path.join(__dirname, '..', 'client');
 app.use(express.static(clientPath));
 app.use(bodyParser.json());
